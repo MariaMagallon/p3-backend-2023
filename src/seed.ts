fb466dbf0e588a338fa876f80fb7f7bc1e2edb5e
MariaMagallon <maria.magallon@estudiantat.upc.edu>
2023-05-31 21:46:09 +0200

    refractor db schema

--- src/seed.ts
+++ src/seed.ts
@@ -1,27 +1,8 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-const singer1 = await prisma.singer.create({
-  data: {
-    fullName: "Beyonce",
-    nacionality: "USA",
-  },
-});
-console.log(`Created singer1 ${singer1.fullName} (${singer1.id})`);
-
-const singer2 = await prisma.singer.create({
-  data: {
-    fullName: "Ed Sheeran",
-    nacionality: "UK",
-  },
-});
-console.log(`Created singer2 ${singer1.fullName} (${singer1.id})`);
+import prisma from "./prisma-client.js";
 
 const genre1 = await prisma.genre.create({
   data: {
     name: "R&B",
-    singerId: singer1.id,
   },
 });
 console.log(`Created genre1 ${genre1.name} (${genre1.id})`);
@@ -29,7 +10,6 @@ console.log(`Created genre1 ${genre1.name} (${genre1.id})`);
 const genre2 = await prisma.genre.create({
   data: {
     name: "Pop",
-    singerId: singer2.id,
   },
 });
 console.log(`Created genre2 ${genre1.name} (${genre1.id})`);
@@ -39,7 +19,6 @@ const album1 = await prisma.album.create({
     title: "Dangerously In Love",
     duration: 63,
     genreId: genre1.id,
-    singerId: singer1.id,
   },
 });
 console.log(`Created album1 ${album1.title} (${album1.id})`);
@@ -49,26 +28,41 @@ const album2 = await prisma.album.create({
     title: "Divide",
     duration: 100,
     genreId: genre2.id,
-    singerId: singer2.id,
   },
 });
 console.log(`Created album2 ${album2.title} (${album2.id})`);
 
-await prisma.song.createMany({
-  data: [
-    {
-      title: "Crazy In Love",
-      duration: 4,
-      genreId: genre1.id,
-      albumId: album1.id,
-      singerId: album1.id,
-    },
-    {
-      title: "Shape of You",
-      duration: 3,
-      genreId: genre2.id,
-      albumId: album2.id,
-      singerId: album2.id,
-    },
-  ],
+const song1 = await prisma.song.create({
+  data: {
+    title: "Crazy In Love",
+    duration: 4,
+    albumId: album1.id,
+  },
+});
+console.log(`Created song1 ${song1.title} (${song1.id})`);
+
+const song2 = await prisma.song.create({
+  data: {
+    title: "Shape of You",
+    duration: 3,
+    albumId: album2.id,
+  },
+});
+console.log(`Created song2 ${song2.title} (${song2.id})`);
+
+await prisma.singer.createMany({
+    data: [
+      {
+        fullName: "Beyonce",
+        nacionality: "USA",
+        songId: song1.id,
+        albumId: album1.id,
+      },
+      {
+        fullName: "Ed Sheeran",
+        nacionality: "UK",
+        songId: song2.id,
+        albumId: album2.id,
+      },
+    ],
 });
