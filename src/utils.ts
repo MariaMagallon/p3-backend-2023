22ea4efc4cebf16af83b57772ae70403f9051d29
MariaMagallon <maria.magallon@estudiantat.upc.edu>
2023-06-05 04:34:37 +0200

    common server and prisma error handler if endpoint validation failed

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,29 @@
 import { ErrorRequestHandler, NextFunction, RequestHandler } from "express";
 import { check, validationResult } from "express-validator";
 
-export const defaultErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+export const defaultErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next
+) => {
   switch (err.constructor.name) {
     case "NotFoundError":
       return res.status(404).json({
         type: err.constructor.name,
         message: `${err.message} for this ID.`,
       });
-    case "PrismaClientKnownRequestError": 
+    case "PrismaClientKnownRequestError":
       return res.status(404).json({
         type: err.constructor.name,
-        message: `A property in requiered body is missing, empty or doesn't exist. Name or title must be unique`,
+        message:
+          "A property in requiered body is missing, empty or not exist. Name or title must be unique",
+      });
+    case "PrismaValidationError":
+      return res.status(404).json({
+        type: err.constructor.name,
+        message:
+          "Check required body params type or structure",
       });
     default:
       console.error(err);
@@ -20,7 +32,7 @@ export const defaultErrorHandler: ErrorRequestHandler = (err, req, res, next) =>
         message: err.message || err.toString(),
       });
   }
-}
+};
 
 export const errorChecked = (handler: RequestHandler): RequestHandler => {
   return async (req, res, next) => {
@@ -30,22 +42,40 @@ export const errorChecked = (handler: RequestHandler): RequestHandler => {
       console.error(e);
       next(e);
     }
-  }
-}
+  };
+};
 
 export const validateIdParam = (): any => {
-  return[
+  return [
     check("id")
       .notEmpty()
-      .exists().withMessage("ID is required")
+      .exists()
+      .withMessage("ID is required")
       .isNumeric()
       .withMessage("ID must be numeric"),
-    (req, res, next): RequestHandler=> {
+    (req, res, next): RequestHandler => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
       next();
     },
-  ]
-} 
+  ];
+};
+
+export const validateGenreParams = (): any => {
+  return [
+    check("name")
+      .notEmpty()
+      .withMessage("Genre name is required")
+      .isString()
+      .withMessage("Genre name must be a string"),
+    (req, res, next): RequestHandler => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    },
+  ];
+};
